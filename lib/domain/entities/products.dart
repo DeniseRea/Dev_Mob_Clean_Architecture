2fdd164e91e2d1e205374a4f4518197f84197c25
Denise Noemi Rea Diaz <105187806+DeniseRea@users.noreply.github.com>
2025-12-02 09:40:08 -0500

    ADD Products

---
+++ lib/domain/entities/products.dart
@@ -0,0 +1,11 @@
+class Products {
+  final String id;
+  final String name;
+  final double valor;
+
+  Products({
+    required this.id,
+    required this.name,
+    required this.valor,
+  });
+}
